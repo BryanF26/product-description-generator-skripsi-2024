7b8f8b2e3d9e820d08d9f2fc65f075f3b2566403
10lalala <bryanfelix2001@gmail.com>
2024-01-24 00:01:51 +0700

    aplikasi 2.0

--- FastApi.py
+++ FastApi.py
@@ -1,37 +1,109 @@
 from fastapi import FastAPI
-from starlette.responses import Response
-# pip install happytransformer
 from happytransformer import HappyGeneration, GENSettings
+import nltk
+from rouge import Rouge
+from nltk.translate.bleu_score import sentence_bleu
+nltk.download('punkt')
 
-def settings():
-    min_length =  50
-    max_length = 100 
+data_store = []
+
+data_description = {
+    'Electronics':[
+        "",
+        "",
+        ""
+    ], 
+    'Home & Kitchen':[
+        "",
+        "",
+        ""
+    ],
+    'Toys & Games':[
+        "",
+        "",
+        ""
+    ]
+}
+
+def settings(min_val= 50, max_val=100, temp=0.7, top_ks=50):
+    min_length =  min_val
+    max_length = max_val
     do_sample = True
     early_stopping = True
     num_beams = 1 
-    temperature = 0.7
-    top_k = 50
-    top_p = 0.7
+    temperature = temp
+    top_k = top_ks
+    top_p = temp
     no_repeat_ngram_size = 2
     gen_args = GENSettings(min_length, max_length, do_sample, early_stopping, num_beams, temperature, top_k, no_repeat_ngram_size, top_p)
     return gen_args
 
+def calculate_rouge_l_f_scores(model_summaries, reference_summaries):
+    rouge = Rouge()
+    scores = rouge.get_scores(model_summaries, reference_summaries)
+    return scores['rouge-l']['f']
+
+def calculate_bleu_scores(model_summaries, reference_summaries):
+    bleu_scores = []
+    for model, reference in zip(model_summaries, reference_summaries):
+        model_tokens = nltk.word_tokenize(model)
+        reference_tokens = [nltk.word_tokenize(reference)]
+        bleu_scores = sentence_bleu(reference_tokens, model_tokens)
+    
+    return bleu_scores
+
 app = FastAPI()
 
-@app.post('/generateGPT2')
-def generate_gpt2(category: str, title: str, feature: str):
+@app.post('/testGPT2')
+def generate_gpt2(category: str, description: str, min_val: int, max_val: int, temps: float, top_ks: int):
+    if category == 'Electronics':
+        data_electronic
+    elif category == 'Home & Kitchen':
+        data_home_and_kitchen
+    elif category == 'Toys & Games':
+        data_toy_and_games
     happy_gen = HappyGeneration(load_path="./GPT 2")
-    gen_args = settings()
+    gen_args = settings(min_val, max_val, temps, top_ks)
     text = f"""Categories: {category}
     Title: {title}
     Features: {feature}
     Description: """
     result = happy_gen.generate_text(text, args=gen_args)
     output = result.text
-    return output
+    try:
+        output = output.split('Description:')[1]
+    except:
+        output = output
+    bleu_score = calculate_bleu_scores(output,description)
+    rouge_score = calculate_rouge_l_f_scores(output, description)
+    return output, bleu_score, rouge_score
 
-@app.post('/generateGPTNeo')
-def generate_gptneo(category:str, title:str, feature:str):
+@app.post('/testGPTNeo')
+def generate_gptneo(category: str, description: str, min_val: int, max_val: int, temps: float, top_ks: int):
+    if category == 'Electronics':
+        data_electronic
+    elif category == 'Home & Kitchen':
+        data_home_and_kitchen
+    elif category == 'Toys & Games':
+        data_toy_and_games
+    happy_gen = HappyGeneration(load_path="./GPT Neo")
+    gen_args = settings(min_val, max_val, temps, top_ks)
+    text = f"""Categories: {category}
+    Title: {title}
+    Features: {feature}
+    Description: """
+    result = happy_gen.generate_text(text, args=gen_args)
+    output = result.text
+    try:
+        output = output.split('Description:')[1]
+    except:
+        output = output
+    bleu_score = calculate_bleu_scores(output,description)
+    rouge_score = calculate_rouge_l_f_scores(output, description)
+    return bleu_score, rouge_score
+
+@app.post('/generateText')
+def generate_gpt2(category: str, title: str, feature: str):
     happy_gen = HappyGeneration(load_path="./GPT Neo")
     gen_args = settings()
     text = f"""Categories: {category}
@@ -40,4 +112,18 @@ def generate_gptneo(category:str, title:str, feature:str):
     Description: """
     result = happy_gen.generate_text(text, args=gen_args)
     output = result.text
+    try:
+        output = output.split('Description:')[1]
+    except:
+        output = output
+    data = [category, title, feature, output]
+    data_store.append(data)
     return output
+
+@app.get('/history')
+def show_history():
+    return data_store
+
+@app.get('/allHistory')
+def delete_all_history():
+    data_store.clear()
--- Frontend.py
+++ Frontend.py
@@ -1,50 +1,95 @@
 import streamlit as st
 import requests
-from requests_toolbelt.multipart.encoder import MultipartEncoder
+import pandas as pd
 
 url = 'http://127.0.0.1:8000'
 
-tab1, tab2, tab3 = st.tabs(["Information", "Generate","Model"])
-
-with tab1:
-    st.title('Introduction')
-
-with tab2:
-    st.title('Text Generation Based On GPT')
-    models = ['GPT 2', 'GPT Neo']
-    selected_model = st.selectbox('Model:', models)
-
-    categories = ['Electronics', 'Home & Kitchen', 'Toys & Games']
-    selected_category = st.selectbox('Category:', categories)
-
-    title_input = st.text_input('Title:')
-
-    feature_input = st.text_area('Feature:')
-
-with tab3:
-    st.title('Model Details')
+def test_model(url, selected_category, selected_description, min_val, max_val, temps, top_ks):
+    text = requests.post(url,
+                        params={
+                            'category':selected_category,
+                            'description': selected_description,
+                            'min_val': min_val,
+                            'max_val': max_val,
+                            'temps': temps,
+                            'top_ks': top_ks
+                        }
+    )
+    return text.json()
 
 def generates(url, selected_category, title_input, feature_input):
     text = requests.post(url,
                         params={
                             'category':selected_category,
                             'title': title_input,
-                            'feature': feature_input
+                            'feature': feature_input,
                         }
     )
     return text.json()
 
-if st.button('Generate Text'):
-    if title_input == "" and feature_input == "":
-        st.write(f'Please fill Title and Features')
-    else:
+
+def test_model_button():
+    if st.button('Test Model'):
         if selected_model == "GPT 2":
-            endpoint = '/generateGPT2'
+            endpoint = '/testGPT2'
         if selected_model == "GPT Neo":
-            endpoint = '/generateGPTNeo'
-        generated_text = generates(url+endpoint, selected_category, title_input, feature_input)
-        try:
-            generated_text = generated_text.split('Description:')[1]
-            st.write(f'Generated Description: {generated_text}')
-        except:
-            st.write(f'Generated Description: {generated_text}')
+            endpoint = '/testGPTNeo'
+        bleu_score, rouge_score = test_model(url+endpoint, selected_category, selected_description, min_val, max_val, temps, top_ks)
+        st.write(f'Generated Bleu Score: {bleu_score}')
+        st.write(f'Generated Rouge-L Score: {rouge_score}')
+
+def generated_text_button():
+    if st.button('Generate Text'):
+        if title_input == "" or feature_input == "":
+            return st.write(f'Please fill Title and Features')
+        generated_text = generates(url+'/generateText', selected_category, title_input, feature_input)
+        st.write(f'Generated Description: {generated_text}')
+
+
+tab1, tab2, tab3, tab4 = st.tabs(["Home", "Test Model", "Generate","History"])
+
+with tab1:
+    st.title('Introduction')
+    st.write("Welcome to our revolutionary website, where the cutting edge of AI-powered creativity meets the ever-changing demands of digital content. Using sophisticated language models, GPT-2 and GPT-Neo, we provide a breakthrough tool for creating high-quality, varied, and interesting writing.")
+    st.write("Our platform is based on the idea of improving human creativity with AI support. Whether you're a marketing searching for compelling product descriptions, our service meets a wide range of text generating of product description.")
+    st.write("We use GPT-2, an advanced language model created by OpenAI that is well-known for its capacity to generate coherent and contextually appropriate text in response to a given prompt. In addition, we introduce GPT-Neo, an open-source alternative that matches the GPT-3 design.")
+    st.write("Our user-friendly interface allows you to easily choose model and category you want and input your product title and feature. With a few clicks, you can produce product description as the outcome.")    
+    
+with tab2:
+    st.title('Testing Model')
+    
+    models = ['GPT 2', 'GPT Neo']
+    selected_model = st.selectbox('Model:', models, key="model")
+    
+    categories = ['Electronics', 'Home & Kitchen', 'Toys & Games']
+    selected_category = st.selectbox('Category:', categories, key="category")
+    
+    description = requests.get(url+'/description').json()
+    selected_description = st.selectbox('Description:', options=[d for d in description[categories]], key="description")
+    
+    min_val = st.slider('Temperature (Unique Word Generate)', min_value=10, max_value=100, value=10)
+    max_val = st.slider('Temperature (Unique Word Generate)', min_value=10, max_value=100, value=50)
+    temps = st.slider('Temperature (Unique Word Generate)', min_value=0.1, max_value=1.0, value=0.7)
+    top_ks = st.slider('Top_K (Possiblity Word Generate)', min_value=0, max_value=100, value=50)
+    
+    test_model_button()
+    
+    
+with tab3:
+    st.expander('Generate',False)
+    st.title('Text Generation')
+    categories = ['Electronics', 'Home & Kitchen', 'Toys & Games']
+    selected_category = st.selectbox('Category:', categories)
+    title_input = st.text_input('Title:')
+    feature_input = st.text_area('Feature:')
+    generated_text_button()
+
+with tab4:
+    st.title('History')
+    data = []
+    data = requests.get(url+'/history').json()
+    df = pd.DataFrame(data,columns=['Category', 'Title', 'Feature', 'Description'])
+    st.dataframe(df, use_container_width=True, hide_index=True)
+    if st.button('Clear History'):
+        requests.get(url+'/allHistory').json()
+        st.rerun()
